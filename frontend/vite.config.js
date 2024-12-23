6149136e0820d8d9258922cb864c256dd2e4756e
gede <ariefartha99@gmail.com>
2024-12-23 23:43:55 +0800

    api edit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     proxy: {
       "/api": {
         target: "https://project-backend-six.vercel.app",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
