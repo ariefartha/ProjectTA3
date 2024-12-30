23e18cba575bcd87807b20fd82f5b376735abfd2
gede <ariefartha99@gmail.com>
2024-12-30 18:01:43 +0800

    cors-error-updated

--- backend/server.js
+++ backend/server.js
@@ -16,53 +16,23 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:5000', 'https://sukaprivatemengemudi.vercel.app'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+const allowedOrigins = ['https://sukaprivatemengemudi.vercel.app'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true 
+}));
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-//cors
-// app.use(
-//   cors({
-//     origin: "https://sukaprivatemengemudi.vercel.app/", 
-//     credentials: true, 
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//   })
-// );
-
-// const allowedOrigins = ['http://localhost:5000', 'https://sukaprivatemengemudi.vercel.app'];
-
-// app.use(cors({
-//   credentials: true,
-//   origin: (origin, callback) => {
-//       if (allowedOrigins.includes(origin) || !origin) {
-//           callback(null, true);
-//       } else {
-//           callback(new Error('Not allowed by CORS'));
-//       }
-//   }
-// }));
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://sukaprivatemengemudi.vercel.app/'); // Ganti dengan domain frontend
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   next();
-// });
-
 
 //midleware
 app.use(express.json({ limit: "50mb" })); //to parse JSON data in the req.body
