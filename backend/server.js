5d6335eae13dc1c3ee619f874d5632531f1debce
gede <ariefartha99@gmail.com>
2024-12-30 18:07:56 +0800

    astungkara

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import cors from "cors"
+import cors from "cors";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
@@ -16,30 +16,41 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Daftar origin yang diizinkan
 const allowedOrigins = ['https://sukaprivatemengemudi.vercel.app'];
+
+// Middleware CORS
 app.use(cors({
   origin: (origin, callback) => {
+    // Izinkan permintaan dari origin yang diizinkan
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true 
+  credentials: true // Izinkan pengiriman kredensial (cookie)
 }));
 
+// Konfigurasi Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-//midleware
-app.use(express.json({ limit: "50mb" })); //to parse JSON data in the req.body
-app.use(express.urlencoded({ extended: true })); //to parse form data in the req.body
-app.use(cookieParser()); //allow get the cookie from request and set the cookie inside response
+// Middleware
+app.use(express.json({ limit: "50mb" })); // Untuk mem-parsing data JSON di req.body
+app.use(express.urlencoded({ extended: true })); // Untuk mem-parsing data form di req.body
+app.use(cookieParser()); // Untuk mendapatkan cookie dari permintaan dan mengatur cookie di respons
 
-//routes
+// Menangani permintaan preflight (OPTIONS)
+app.options('*', cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
+// Routes
 app.get("/", (req, res) => {
   res.send("my server is running");
 });
@@ -49,4 +60,5 @@ app.use("/api/testimoni", testimoniRoutes);
 app.use("/api/schedule", scheduleRoutes);
 app.use("/api/payment", paymentRoutes);
 
+// Menjalankan server
 app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
