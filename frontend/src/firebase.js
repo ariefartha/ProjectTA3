7c5cee740fa0eecd01b514ef34d5803bedea75e5
gede <ariefartha99@gmail.com>
2025-01-01 23:48:15 +0800

    ENV-UPDATED

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAErqlHzoTlG5m8dCCn1dwcINoIardWQb8",
-  authDomain: "sukaprivatemengemudi.firebaseapp.com",
-  projectId: "sukaprivatemengemudi",
-  storageBucket: "sukaprivatemengemudi.firebasestorage.app",
-  messagingSenderId: "1047142941214",
-  appId: "1:1047142941214:web:682b2d68b7efbe59cae40f"
+  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
